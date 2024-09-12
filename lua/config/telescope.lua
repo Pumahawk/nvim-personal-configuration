7d4a7ea6a6a41e9da71ed2b8c03d15614cd2d696
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-09-12 19:07:36 +0200

    add telescope lsp keymap

--- lua/config/telescope.lua
+++ lua/config/telescope.lua
@@ -13,3 +13,9 @@ vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
 vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>gc', builtin.git_bcommits, {})
+
+-- Lsp
+vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>sw', builtin.lsp_workspace_symbols, {})
+vim.keymap.set('n', '<leader>r', builtin.lsp_references, {})
+

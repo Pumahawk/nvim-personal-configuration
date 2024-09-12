ac6c39e76bfafee12bd5451fe8073976f981627f
Lorenzo Gandino <lorenzoga94@gmail.com>
2024-09-12 19:07:54 +0200

    add LSP Docker go lua

--- lua/config/lsp-custom.lua
+++ lua/config/lsp-custom.lua
@@ -5,3 +5,15 @@ require('lspconfig').jdtls.setup({})
 require'lspconfig'.html.setup{}
 require'lspconfig'.ts_ls.setup{}
 require'lspconfig'.eslint.setup{}
+require'lspconfig'.angularls.setup{}
+
+-- Docker
+require'lspconfig'.dockerls.setup{}
+require'lspconfig'.docker_compose_language_service.setup{}
+
+-- Go
+require'lspconfig'.gopls.setup{}
+
+-- Lua
+require'lspconfig'.lua_ls.setup{}
+

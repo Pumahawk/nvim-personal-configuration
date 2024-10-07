1d9ba324cf42d691038804f48d2729b55db3bb89
Lorenzo Gandino <lorenzo.gandino@alten.it>
2024-10-08 01:18:58 +0200

    Comment center after movements

--- lua/config/custom.lua
+++ lua/config/custom.lua
@@ -18,9 +18,9 @@ vim.keymap.set('n', '<leader>xs', ':Sex<CR>', {})
 vim.keymap.set('n', '<leader>xv', ':Vex<CR>', {})
 
 -- Jump and center
-vim.keymap.set('n', 'n', 'nzz', {})
-vim.keymap.set('n', 'N', 'Nzz', {})
-vim.keymap.set('n', '<C-o>', '<C-o>zz', {})
-vim.keymap.set('n', '<C-i>', '<C-i>zz', {})
+-- vim.keymap.set('n', 'n', 'nzz', {})
+-- vim.keymap.set('n', 'N', 'Nzz', {})
+-- vim.keymap.set('n', '<C-o>', '<C-o>zz', {})
+-- vim.keymap.set('n', '<C-i>', '<C-i>zz', {})
 
 
